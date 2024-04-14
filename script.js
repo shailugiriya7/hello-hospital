09fc21e13eb2822a0a4fa10f003f7c195a866688
shailesh Giriya <142213894+shailugiriya7@users.noreply.github.com>
2024-04-15 03:52:23 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+
+let nav=document.querySelector(".navbar");
+
+window.onscroll=function(){
+    if (document.documentElement.scrollTop>100) {
+        nav.classList.add("header-scrolled");
+    }
+    else{
+        nav.classList.remove("header-scrolled");
+    }
+}
+
+let navBar=document.querySelectorAll(".nav-link");
+let navCollapse=document.querySelector(".navbar-collapse.collapse");
+
+navBar.forEach((e)=>{
+e.addEventListener("click",()=>{
+    navCollapse.classList.remove("show");
+})
+})
